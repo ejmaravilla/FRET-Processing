297d55cb3f659c8277e035806bfa65b7958e0841
bhoffmanlab <bhoffmanlab@gmail.com>
2014-05-07 15:10:20 -0400

    Update header & function

--- src/file_search.m
+++ src/file_search.m
@@ -1,14 +1,16 @@
-function file_matches = file_search(exp,folder)
+function filecell = file_search(exp,folder)
+
+% a function to find files of a given expression in a particular folder
+
 listing = dir(folder);
-listing(1:2) = [];
+listing(1:2)=[];
 files = {listing.name};
 
-% match all regular expressions
-% indi = cellfun(@(x)(~isempty(x)),regexp(files,exp));
 match = regexp(files,exp);
-file_matches = cell(0);
+indi = [];
 for i = 1:length(match)
     if match{i}==1
-        file_matches{end+1} = fullfile(folder,files{i}); %#ok<AGROW>
+        indi = [indi i];
     end
 end
+filecell = files(indi);
