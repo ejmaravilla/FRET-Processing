c30fc4eb4b85ecb250539380070ed5909fe1ed17
bhoffmanlab <bhoffmanlab@gmail.com>
2014-05-07 15:28:47 -0400

    Aesthetic changes

--- src/spl_interp_k.m
+++ src/spl_interp_k.m
@@ -1,26 +1,24 @@
 function y = spl_interp_k(xa,ya,y2a,x)
 
-[r c] = size(xa);
+[r,c] = size(xa);
 if c ~=1
     xa = xa';
 end
-[r c] = size(ya);
+[r,c] = size(ya);
 if c ~=1
     ya = ya';
 end
-[r c] = size(x);
+[r,c] = size(x);
 if c ~=1
     x = x';
 end
-[r c] = size(y2a);
+[r,c] = size(y2a);
 if c ~=1
     y2a = y2a';
 end
 
 n = length(xa);
-% klo = VALUE_LOCATE(xa,x) > 0L < (n-2)
 [~, klo] = histc(x,xa);
-% klo(klo < 0) = 0;
 klo(klo < 1) = 1;
 klo(klo > (n-1)) = n-1;
 
