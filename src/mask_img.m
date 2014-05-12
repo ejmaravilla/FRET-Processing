2f25f005e933b2e4178c3113cbaa6ceb3e0b53e7
bhoffmanlab <bhoffmanlab@gmail.com>
2014-05-12 13:24:22 -0400

    Added function to make cell masks

---
+++ src/mask_img.m
@@ -0,0 +1,20 @@
+function mask_img(filename,folder)
+
+% saves masks corresponding to cells (drawn using boundary_dist) in single
+% image
+
+mask_names = file_search(filename,folder);
+num_masks = length(mask_names);
+temp = imread(mask_names{1});
+img_size = size(temp);
+masks = zeros(img_size);
+figure(1); hold on;
+for i = 1:num_masks
+    mask = imread(mask_names{i});
+    masks = masks+i*mask;
+end
+
+imwrite2tif(masks,[],fullfile(folder,['polymask_cells_' mask_names{1}(16:end)]),'single');
+
+end
+
