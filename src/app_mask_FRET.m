7b0923d5796345cc06b124c690e01d674c69ea95
alacroix367 <alacroix367@gmail.com>
2014-05-14 09:28:51 -0400

    Mask correct images

--- src/app_mask_FRET.m
+++ src/app_mask_FRET.m
@@ -7,15 +7,15 @@ for i = 1:length(files)
     scores = strfind(files{i},'_');
     base = files{i}(4:scores(end));
     
-    C1 = single(imread([base 'w1' A '.TIF']));
+    C1 = single(imread(['bsa_' base 'w1' A '.TIF']));
     mC1 = m.*C1;
     imwrite2tif(mC1,[],fullfile(folder,['masked_' base 'w1' A '.TIF']),'single')
     
-    C2 = single(imread([base 'w2' FRET '.TIF']));
+    C2 = single(imread(['cna_' base 'w2' FRET '.TIF']));
     mC2 = m.*C2;
     imwrite2tif(mC2,[],fullfile(folder,['masked_' base 'w2' FRET '.TIF']),'single')
     
-    C3 = single(imread([base 'w3' D '.TIF']));
+    C3 = single(imread(['bsd_' base 'w3' D '.TIF']));
     mC3 = m.*C3;
     imwrite2tif(mC3,[],fullfile(folder,['masked_' base 'w3' D '.TIF']),'single')
 end
