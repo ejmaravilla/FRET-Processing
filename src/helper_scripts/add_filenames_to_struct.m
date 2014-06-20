93819b905588451d4728f1c2be2db5676c97e2a2
Matthew Berginski <matthew.berginski@gmail.com>
2014-06-20 11:40:29 -0400

    removing unused helper_scripts folder

--- src/helper_scripts/add_filenames_to_struct.m
+++
@@ -1,9 +0,0 @@
-function filenames = add_filenames_to_struct(filenames)
-%ADD_FILENAMES_TO_STRUCT    Takes in a struct, to which the filenames for
-%                           various files made in the pipeline are added
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Main Program
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-filenames.images = 'images';
--- src/helper_scripts/mkdir_no_err.m
+++
@@ -1,5 +0,0 @@
-function mkdir_with_create(dir)
-
-if not(exist(dir,'dir'))
-    mkdir(dir);
-end
