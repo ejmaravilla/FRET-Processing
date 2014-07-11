96cc515c375a2d1b86ef7ec9f72415c51406e838
alacroix367 <alacroix367@gmail.com>
2014-07-11 10:29:00 -0400

    Corrected naming scheme

--- src/app_mask_FRET.m
+++ src/app_mask_FRET.m
@@ -9,13 +9,13 @@ for i = 1:length(files)
     
     C1 = single(imread(['bsa_' base 'w1' A '.TIF']));
     mC1 = m.*C1;
-    imwrite2tif(mC1,[],fullfile(folder,['masked_' base 'w1' A '.TIF']),'single')
+    imwrite2tif(mC1,[],fullfile(folder,['masked_bsa_' base 'w1' A '.TIF']),'single')
     
     C2 = single(imread(['cna_' base 'w2' FRET '.TIF']));
     mC2 = m.*C2;
-    imwrite2tif(mC2,[],fullfile(folder,['masked_' base 'w2' FRET '.TIF']),'single')
+    imwrite2tif(mC2,[],fullfile(folder,['masked_cna_' base 'w2' FRET '.TIF']),'single')
     
     C3 = single(imread(['bsd_' base 'w3' D '.TIF']));
     mC3 = m.*C3;
-    imwrite2tif(mC3,[],fullfile(folder,['masked_' base 'w3' D '.TIF']),'single')
+    imwrite2tif(mC3,[],fullfile(folder,['masked_bsd_' base 'w3' D '.TIF']),'single')
 end
