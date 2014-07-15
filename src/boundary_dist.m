a8ac10c3e252c91836f9abf7177e422f00059075
alacroix367 <alacroix367@gmail.com>
2014-07-15 08:40:31 -0400

    Now works with automatic cell selection using cell_outline_simple

--- src/boundary_dist.m
+++ src/boundary_dist.m
@@ -1,7 +1,7 @@
 function newcols = boundary_dist(imgexp,filename,folder,closed_open,manual,reg_calc,rat,pre_exist,num_channel)
 
 % This function calculates the distance from the edge of provided blobs. It
-% functions on manually drawing closed/open polygons, providing pre-defined 
+% functions on manually drawing closed/open polygons, providing pre-defined
 % polygons (must be closed?),or automatically generating closed polygons.
 
 img_col = 2*num_channel+7;
@@ -35,7 +35,6 @@ if strcmpi(closed_open,'closed') && strcmpi(manual,'y') && strcmpi(pre_exist,'n'
         cell_convex_area = zeros(length(rows),cell_num);
         cell_per = zeros(length(rows),cell_num);
         cell_center_dist = zeros(length(rows),cell_num);
-        
         for k = 1:cell_num
             v = 1;
             while v == 1;
@@ -72,7 +71,7 @@ if strcmpi(closed_open,'closed') && strcmpi(manual,'y') && strcmpi(pre_exist,'n'
             cell_col(rows) = sum(cell_col_img,2); % Fix for overlapping boundaries
             dist_col(rows) = sum(dists_img,2);
             close all;
-            newcols = [dist_col cell_col];        
+            newcols = [dist_col cell_col];
         end
     end
     
@@ -92,7 +91,6 @@ elseif strcmpi(closed_open,'open') && strcmpi(manual,'y') && strcmpi(pre_exist,'
             M = imfreehand(gca,'Closed',0);
             v = input('Keep region (yes = pressENTER, no = press1)?');
         end
-%         M = imfreehand(gca,'Closed',0);
         P0 = M.getPosition;
         D = round([0; cumsum(sum(abs(diff(P0)),2))]);
         P = interp1(D,P0,D(1):.5:D(end));
@@ -112,37 +110,59 @@ elseif strcmpi(closed_open,'open') && strcmpi(manual,'y') && strcmpi(pre_exist,'
     
 elseif strcmpi(closed_open,'closed') && strcmpi(manual,'n') && strcmpi(pre_exist,'n')
     files = file_search(imgexp,folder);
-%     im = imread(imgexp);
     d = load(filename);
     [m,~] = size(d);
     u = unique(d(:,img_col));
     [o,~] = size(u);
     cell_col = zeros(m,1);
     dist_col = zeros(m,1);
-    idealSquareSize = 4;
+    cell_area_col = zeros(m,1);
+    cell_ecc_col = zeros(m,1);
+    cell_cent_x_col = zeros(m,1);
+    cell_cent_y_col = zeros(m,1);
+    cell_convex_area_col = zeros(m,1);
+    cell_per_col = zeros(m,1);
+    cell_center_dist_col = zeros(m,1);
     ratioThresh = rat;
-    sizeThresh = 1/100;
     for i = 1:o
-        P0 = CellSelectorFunction1(files{i},idealSquareSize,ratioThresh,sizeThresh);
+        P0 = cell_outline_simple(files{i},ratioThresh,folder);
+        rehash
         cell_num = length(P0);
         rows = find(d(:,img_col)==i);
         dists_img = zeros(length(rows),cell_num);
         cell_col_img = zeros(length(rows),cell_num);
+        cell_area = zeros(length(rows),cell_num);
+        cell_ecc = zeros(length(rows),cell_num);
+        cell_cent_x = zeros(length(rows),cell_num);
+        cell_cent_y = zeros(length(rows),cell_num);
+        cell_convex_area = zeros(length(rows),cell_num);
+        cell_per = zeros(length(rows),cell_num);
+        cell_center_dist = zeros(length(rows),cell_num);
         for k = 1:cell_num
-            P1 = P0{k};
-%             P1 = round([0; cumsum(sum(abs(diff(P2)),2))]);
-%             P = interp1(D,P1,D(1):.5:D(end));
-            mask1 = poly2mask(P1(:,1), P1(:,2), 1948, 1948);
-            mask = mat2gray(mask1);
-            imwrite2tif(mask,[],fullfile(folder,['polymask_cell' num2str(k) '_' files{i}]),'single');
-            save(fullfile(pwd,folder,['poly_cell' num2str(k) '_' files{i}(1:end-4) '.dat']),'P1','-ascii')
-            rehash
-            [cell_col_img(:,k),dists_img(:,k)] = app_poly_blobs(filename,fullfile(pwd,folder,['poly_cell' num2str(k) '_' files{i}(1:end-4) '.dat']),i,k,num_channel);
+            if strcmpi(reg_calc,'y')
+                [cell_col_img(:,k),dists_img(:,k),cell_area(:,k),cell_ecc(:,k), cell_cent_x(:,k), cell_cent_y(:,k), cell_convex_area(:,k), cell_per(:,k), cell_center_dist(:,k)] = app_poly_blobs_cells(filename,fullfile(pwd,folder,['poly_cell' num2str(k) '_' files{i}(1:end-4) '.dat']),['polymask_cell' num2str(k) '_' files{i}],i,k,num_channel);
+            elseif strcmpi(reg_calc,'n')
+                [cell_col_img(:,k),dists_img(:,k)] = app_poly_blobs(filename,fullfile(pwd,folder,['poly_cell' num2str(k) '_' files{i}(1:end-4) '.dat']),i,k,num_channel);
+            end
         end
-        cell_col(rows) = sum(cell_col_img,2); % Fix for overlapping boundaries
-        dist_col(rows) = sum(dists_img,2);
-        close all;
-        newcols = [dist_col cell_col];
+        if strcmpi(reg_calc,'y')
+            cell_col(rows) = sum(cell_col_img,2); % Fix for overlapping boundaries
+            dist_col(rows) = sum(dists_img,2);
+            cell_area_col(rows) = sum(cell_area,2);
+            cell_ecc_col(rows) = sum(cell_ecc,2);
+            cell_cent_x_col(rows) = sum(cell_cent_x,2);
+            cell_cent_y_col(rows) = sum(cell_cent_y,2);
+            cell_convex_area_col(rows) = sum(cell_convex_area,2);
+            cell_per_col(rows) = sum(cell_per,2);
+            cell_center_dist_col(rows) = sum(cell_center_dist,2);
+            close all;
+            newcols = [dist_col cell_col cell_area_col cell_ecc_col cell_cent_x_col cell_cent_y_col cell_convex_area_col cell_per_col cell_center_dist_col];
+        elseif strcmpi(reg_calc,'n')
+            cell_col(rows) = sum(cell_col_img,2); % Fix for overlapping boundaries
+            dist_col(rows) = sum(dists_img,2);
+            close all;
+            newcols = [dist_col cell_col];
+        end
     end
     
 elseif strcmpi(pre_exist,'y')
@@ -203,7 +223,7 @@ elseif strcmpi(pre_exist,'y')
             cell_col(rows) = sum(cell_col_img,2); % Fix for overlapping boundaries
             dist_col(rows) = sum(dists_img,2);
             close all;
-            newcols = [dist_col cell_col];        
+            newcols = [dist_col cell_col];
         end
-    end    
+    end
 end
