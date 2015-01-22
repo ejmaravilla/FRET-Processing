10e3e830dfbbe7cab315977fab4247f5cfa4d26a
alacroix367 <alacroix367@gmail.com>
2015-01-21 19:02:54 -0500

    Added option to subtract single value from images if using FA_gen.m on stain images (non-Venus)

--- src/fa_gen.m
+++ src/fa_gen.m
@@ -1,12 +1,25 @@
-function fa_gen(fname,params,fold)
+function fa_gen(fname,params,fold,SaveParams)
 
 % A simple program to generate masks using the water program
-% A typical set of params is [25,1000,25]
+% A typical set of params is [25,1000,25]. If you input a bkg intensity, it
+% will be subtracted off all images before fa_gen
 
 files = file_search(fname,fold);
 
 for i = 1:length(files)
     img = double(imread(files{i}));
+% Only performs background subtraction on non-Venus channels
+    if isfield(SaveParams,'bkg') && isempty(strfind(files{i},'Venus'))
+        img = (img-SaveParams.bkg);
+        img(img < 0) = 0;
+    elseif isfield(SaveParams,'bkg1') && isempty(strfind(files{i},'Venus'))
+        img = (img - SaveParams.bkg1);
+        img(img < 0) = 0;
+    elseif isfield(SaveParams,'bkg2') && isempty(strfind(files{i},'Venus'))
+        img = (img - SaveParams.bkg2);
+        img(img < 0) = 0;
+    end
+    % Resume water on possibly background subtracted image
     w = water(img,params);
     imwrite2tif(w,[],fullfile(fold,['fa_' files{i}]),'single');
 end
