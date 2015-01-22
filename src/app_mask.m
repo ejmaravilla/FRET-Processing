97c753673bad5c772ca77e4f5bbe3b85e64f04b5
alacroix367 <alacroix367@gmail.com>
2015-01-21 19:01:34 -0500

    Universal app_mask (designed off blob_analyze.m)

--- src/app_mask.m
+++ src/app_mask.m
@@ -1,18 +1,72 @@
-function app_mask(folder)
+function app_mask(bases,SaveParams,maskchannel)
 
-files = file_search('fa_\w+.TIF',folder);
-for i = 1:length(files)
-    m = double(imread(files{i}));
-    m(m ~= 0) = 1;
-    scores = strfind(files{i},'_');
-    base = files{i}(8:scores(end));
-    cna = single(imread(['cna_' base 'w2TVFRET.TIF']));
-    mcna = m.*cna;
-    imwrite2tif(mcna,[],fullfile(folder,['cna_masked_' base 'w2TVFRET.TIF']),'single')
-    bsa = single(imread(['bsa_' base 'w1Venus.TIF']));
-    mbsa = m.*bsa;
-    imwrite2tif(mbsa,[],fullfile(folder,['bsa_masked_' base 'w1Venus.TIF']),'single')
-    bsd = single(imread(['bsd_' base 'w3Teal.TIF']));
-    mbsd = m.*bsd;
-    imwrite2tif(mbsd,[],fullfile(folder,['bsd_masked_' base 'w3Teal.TIF']),'single')
+% A function to apply a mask to different
+% channels using a provided mask.
+% Inputs:
+%   bases - a cell array containing regular expressions representing each
+%   image channel that you would like to mask separated by commas, with
+%   the last one being the regular expression for the masks (can use the
+%   results of fa_gen)
+%   SaveParams - a structure containing necessary parameters
+%       SaveParams.folder - name of the folder where images are contained
+% Outputs:
+%   masked images
+%       A set of images beginning with avg_ that are the masked input
+%       images with the values contained in each blob averaged together to
+%       create a mean intensity for each blob.
+% Sample Call:
+%   SaveParams.folder = 'FRET_test';
+%   app_mask_new({...
+%                ['cna_' prefix SaveParams.exp_cell{i} '\w+' SaveParams.FRETchannel '.TIF'],...
+%                 ['bsd_' prefix SaveParams.exp_cell{i} '\w+' SaveParams.Dchannel '.TIF'],...
+%                 ['bsa_' prefix SaveParams.exp_cell{i} '\w+' SaveParams.Achannel '.TIF'],...
+%                 ['fa_bsa_' prefix SaveParams.exp_cell{i} '\w+.TIF']},SaveParams)
+%   This applies the masks of the form ['fa_bsa_' prefix SaveParams.exp_cell{i} '\w+.TIF'] to the
+%   three image channels (CNA, BSD, BSA), all found in the folder 'FRET_test'. 
+%   It will output masked images from the three input channels.
+% Required Functions:
+%   file_search
+%   imwrite2tif
+%
+% This code 'app_mask_new' should be considered 'freeware' and may be
+% distributed freely (outside of the military-industrial complex) in its
+% original form when properly attributed.
+
+imgn = imgn_check(bases,SaveParams.folder);
+szn = size(imgn);
+nch = szn(2);
+nt = szn(1);
+
+for i = 1:nt % for each image (nt = number of timepoints or images)
+    imgarr = read_chnls(imgn(i,:));
+    
+    cimg = imgarr{nch-1};
+    bimg = imgarr{nch}; % blob mask image
+    bimg(bimg>0) = 1; % make blob mask image binary
+    for j = 1:nch-1
+        cimg = imgarr{j};
+        cimg_masked = cimg.*bimg;
+        name = fullfile(SaveParams.folder,['masked_on_' maskchannel '_' imgn{i,j}]);
+        imwrite2tif(cimg_masked,[],name,'single');
+    end
 end
+
+end
+
+function imgn = imgn_check(bases,folder)
+results1 = file_search(bases{1},folder);
+imgn = cell(length(results1),length(bases));
+imgn(:,1) = results1;
+for i = 2:length(bases)
+    results = file_search(bases{i},folder);
+    imgn(:,i) = results;
+end
+end
+
+function imgarr = read_chnls(imgncol)
+imgarr = cell(1,length(imgncol));
+for i = 1:length(imgncol);
+    imgarr{i} = double(imread(imgncol{i}));
+end
+end
+
