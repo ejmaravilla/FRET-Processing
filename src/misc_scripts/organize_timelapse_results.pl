2100b6eaaf64d8849c3a21bb6554ad69b5c444db
Matthew Berginski <matthew.berginski@gmail.com>
2015-05-12 10:58:06 -0400

    fixing problem with leading zeros on time images

--- src/misc_scripts/organize_timelapse_results.pl
+++ src/misc_scripts/organize_timelapse_results.pl
@@ -8,6 +8,7 @@ use strict;
 use File::Path qw(make_path);
 use File::Basename;
 use File::Copy;
+use Data::Dumper;
 
 if (scalar(@ARGV) == 0) {
 	die "Expected to see one parameter, the results dir."
@@ -20,7 +21,7 @@ if (scalar(@ARGV) == 0) {
 my $results_dir = shift @ARGV;
 die "Couldn't Find $results_dir" if (! -e $results_dir);
 
-my @files = <$results_dir*>;
+my @files = <"$results_dir*">;
 
 my $images_dir = "$results_dir/images";
 make_path($images_dir);
@@ -28,12 +29,20 @@ make_path($images_dir);
 for (@files) {
 	next if -d $_;
 	
-	print "$_\n";
-	
 	move($_,"$images_dir");
 }
 
-@files = <$images_dir/*>;
+@files = <"$images_dir/*">;
+#Fix sorting problem with leading zero on time slot
+for (@files) {
+	my $filename = basename($_);
+	if ($filename =~ /(.*_w.*_s\d+_t)(\d)(\.TIF)/) {
+		my $fixed_filename = sprintf('%s%02d%s',$1,$2,$3);
+		move($_, "$images_dir/$fixed_filename");
+	}
+}
+
+@files = <"$images_dir/*">;
 
 my @unrefed_files;
 
@@ -49,6 +58,10 @@ for (@files) {
 		system("ln -s \"../../../images/$filename\" \"$target_folder/$filename\"\n");
 	} elsif ($_ =~ /bsd_pre_(.*?)_w.*_s(\d+)_t\d+/) {
 		my $target_folder = "$results_dir/$1/$2/Donor";
+		make_path("$target_folder");
+		system("ln -s \"../../../images/$filename\" \"$target_folder/$filename\"\n");
+	} elsif ($_ =~ /pre_(.*?)_w.*FWTR_s(\d+)_t\d+/) {
+		my $target_folder = "$results_dir/$1/$2/Cherry";
 		make_path("$target_folder");
 		system("ln -s \"../../../images/$filename\" \"$target_folder/$filename\"\n");
 	} elsif ($_ =~ /SaveParams/) {
