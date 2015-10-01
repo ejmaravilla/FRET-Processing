1839e86c9e13921581b14d0cf9f0ce75fd2b98f0
HoffmanLab <bhoffmanlab@gmail.com>
2015-10-01 16:02:39 -0400

    adding new bleedthroughs from Katy's calcs and G/K

--- src/make_experiment_properties_config.m
+++ src/make_experiment_properties_config.m
@@ -1,14 +1,15 @@
 function SaveParams = make_experiment_properties_config
 
-SaveParams.folder = '../results/3-31-2015 Blebbistatin 50uM/';
+SaveParams.folder = '../results/9-14-2015 Blebbistatin/';
 
 % General Experiment Properties
 % Enter the names at the beginning of each type of experiment you want to
 % assess
-SaveParams.exp_cell = {'EZR-TS_no_bleb','EZR-CTS_no_bleb','EZR-TS_bleb','EZR-CTS_bleb'};
+% SaveParams.exp_cell = {'EZR-TS'};
+SaveParams.exp_cell = {'EZR-CTS_pre','EZR-TS_pre', 'EZR-CTS_post','EZR-TS_post'};
 
 SaveParams.num_exp = length(SaveParams.exp_cell);
-    
+
 SaveParams.num_channel = 3;
 
 %These parameters will be used to find the file that is the acceptor
@@ -31,16 +32,20 @@ SaveParams.athres = 800;
 %(acceptor), 1500 (FRET) exposure times
 % SaveParams.abt = 0.26;
 % SaveParams.dbt = 0.96;
-    
+
 %These settings work for Teal-Venus FRET with 500 (donor), 500 (acceptor),
-%500 (FRET) exposure times 
-SaveParams.abt = 0.18;
-SaveParams.dbt = 0.90;
+%500 (FRET) exposure times
+SaveParams.abt = 0.19;
+SaveParams.dbt = 1.03;
 
 
 %FRET Correction Settings
 % -correct: should we do FRET correction ('y' or 'n')
 SaveParams.correct = 'y';
 SaveParams.venus_thres = 100;
+
+% %G and k settings for FRET Efficiency at 1000-1000-1500
+% SaveParams.G = 2.95;
+% SaveParams.k = 0.66;
 
 save(fullfile(SaveParams.folder,'SaveParams.mat'),'-struct','SaveParams');
