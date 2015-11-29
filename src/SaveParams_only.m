032211a47826f2d5f025f7f0136b7786c2ef7711
bhoffmanlab <bhoffmanlab@gmail.com>
2015-11-29 14:08:18 -0500

    Fixed to account for no blob_params

--- src/SaveParams_only.m
+++ src/SaveParams_only.m
@@ -2,7 +2,9 @@ rehash
 params_file = file_search('Exp_Param\w+.txt',folder);
 fid = fopen(fullfile(folder,params_file{1}),'a');
 fprintf(fid,'\n\nfolder = ''%s'';',folder);
-fprintf(fid,'\nfinal_blob_params = [%d %d %d];',blob_params(1),blob_params(2),blob_params(3));
+if exist('blob_params','var')
+    fprintf(fid,'\nfinal_blob_params = [%d %d %d];',blob_params(1),blob_params(2),blob_params(3));
+end
 if exist('cell_thresh','var')
     fprintf(fid,'\ncell_thresh = %d;',cell_thresh*10000);
 end
